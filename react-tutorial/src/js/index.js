8bd33db717083d81b202e15d94c317bf88f0022f
Recursif <jillian.dum@gmail.com>
2018-05-26 21:51:49 +0200

    change the square to props that heritate from Board

--- react-tutorial/src/js/index.js
+++ react-tutorial/src/js/index.js
@@ -6,16 +6,10 @@ import './style.css'
 
 
 class Square extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      value: null,
-    }
-  }
   render() {
     return (
-      <button className="square" onClick={() => this.setState({value: 'X'})}>
-        {this.state.value}
+      <button className="square" onClick={() => this.props.onClick()}>
+        {this.props.value}
       </button>
     )
   }
