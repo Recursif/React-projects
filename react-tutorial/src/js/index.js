f0b2f7ce0d9fbc47068055cab85e15301e8d674b
Recursif <jillian.dum@gmail.com>
2018-05-26 21:02:27 +0200

    repair setState value = X on click

--- react-tutorial/src/js/index.js
+++ react-tutorial/src/js/index.js
@@ -6,10 +6,16 @@ import './style.css'
 
 
 class Square extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      value: null,
+    }
+  }
   render() {
     return (
-      <button className="square" onClick={() => alert('click')}>
-        {this.props.value}
+      <button className="square" onClick={() => this.setState({value: 'X'})}>
+        {this.state.value}
       </button>
     )
   }
