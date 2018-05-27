dfbdc45800c70ce483e50bbe25dcea9c0701254e
Recursif <jillian.dum@gmail.com>
2018-05-27 13:24:20 +0200

    add state in Board instead of each squares

--- react-tutorial/src/js/index.js
+++ react-tutorial/src/js/index.js
@@ -24,8 +24,17 @@ class Board extends Component {
     }
   }
 
+  handleClick(i) {
+    const squares = this.state.squares.slice()
+    squares[i] = 'X'
+    this.setState({squares: squares})
+  }
+
   renderSquare(i) {
-    return <Square value={this.state.squares[i]} />
+    return <Square
+      value={this.state.squares[i]}
+      onClick={() => this.handleClick(i)}
+     />
   }
 
   render() {
