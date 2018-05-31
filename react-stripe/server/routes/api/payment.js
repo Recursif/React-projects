a5e68a2ab30578ce4b56b3ae6fce8081563cc408
Recursif <jillian.dum@gmail.com>
2018-05-31 16:42:06 +0200

    add basic front

--- react-stripe/server/routes/api/payment.js
+++ react-stripe/server/routes/api/payment.js
@@ -4,13 +4,19 @@ const config = require('../../../config/config')
 module.exports = (app) => {
   // Returns the fields needed.
   app.post('/api/stripe/account/get', function (req, res, next) {
-      const stripeAccountId = '';
+      const stripeAccountId = 'test';
 
       if (!stripeAccountId) {
         res.send({
-          success: true,
-          message: 'Missing stripe account.',
-          setupBegan: false,
+          "success": false,
+          "message": 'Missing stripe account.',
+          "setupBegan": false,
+        })
+      } else {
+        res.send({
+          "success": true,
+          "message": 'Stripe account.',
+          "setupBegan": true,
         })
       }
   });
